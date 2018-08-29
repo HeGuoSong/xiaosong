f4f9dcac6dcf514b2f3574ba354147cb88ab232c
HeGuoSong <836963281@qq.com>
2018-08-29 16:03:02 +0800

    helloword

--- framework/conf/default.py
+++ framework/conf/default.py
@@ -31,7 +31,7 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'hellosong'
-APP_TOKEN = '2e94c2e5-15f7-431a-813f-a7692e81ba98'
+APP_TOKEN = '1f8be995-b09b-4e5b-8d0d-ed60cacd1063'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas.blueking.com'
 
